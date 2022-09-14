711b580225e2fbf2e6884beb27d6f4e015890d5e
Chris Grimmett <chris@grimtech.net>
2022-09-14 13:59:12 -0700

    schedule fix

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
 const { getRandomCardListings, pluckInterestingData } = require('./ebay');
 const { doMakeTweet } = require('./twitter');
 const envImport = require('@grimtech/envimport');
-const schedule = envImport('TWEET_SCHEDULE');
 const scheduler = require('node-schedule');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const debug = require('debug')('tweetbot');
+
+ // # ┌────────────── second (optional)
+ // # │ ┌──────────── minute
+ // # │ │ ┌────────── hour
+ // # │ │ │ ┌──────── day of month
+ // # │ │ │ │ ┌────── month
+ // # │ │ │ │ │ ┌──── day of week
+ // # │ │ │ │ │ │
+ // # │ │ │ │ │ │
+ // # * * * * * *
+
+
+const schedule = envImport('TWEET_SCHEDULE');
 var lastTweet = null;
 var lastRunDate = null;
 var lastProduct = null;
@@ -39,6 +51,7 @@ if (argv.oneshot) {
 } else {
   // long-running service
   const job = scheduler.scheduleJob(schedule, main);
+
   console.log(`Tweetbot is running using schedule definition ${schedule}.\nNext invocation is at ${job.nextInvocation()}`);
 
   app.get('/', (req, res) => {
